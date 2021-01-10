cdc8e25665bfdae6eba36a740a031aba5231f8cd
WooJunKang <woojiworld@gmail.com>
2021-01-10 14:24:33 +0900

    modify: edit page scroll bar position

--- client/src/pages/EditFile.jsx
+++ client/src/pages/EditFile.jsx
@@ -1,22 +1,44 @@
 
-import { EditInput, PageDesc, PageHeader, EditSubmitBtn } from '../components';
+import { EditInput, PageDesc, PageHeader, EditBtn } from '../components';
 
-export default function SelectFile({ currentFile, fileData, onSubmit, onChange }) {
+export default function SelectFile({ currentFile, fileData, handleSubmit, dispatch }) {
   console.log('file data is', fileData)
   const keys = Object.keys(fileData)
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // console.log('btn type: ', e.target)
+    handleSubmit(fileData)
+  }
   return (
     <>
       <PageHeader title={"파일 수정"} />
       <PageDesc desc={`현재 파일: ${currentFile || ""}`} />
-      <form onSubmit={onSubmit}>
-        <EditSubmitBtn />
-        <ul>
-          {
-            keys.map(key => {
-              return <EditInput key={key} label={key} value={fileData[key]} onChange={onChange} />
-            })
-          }
-        </ul>
+      <form
+        className="flex-col-center"
+        onSubmit={onSubmit}
+      >
+
+        {
+          currentFile && (
+            <>
+              <div className="flex-row-between" style={{ width: "10rem" }}>
+                <EditBtn btnType="SUBMIT" />
+                <EditBtn btnType="CANCEL" />
+              </div>
+              <div className="form-contents-container" style={{ height: "70vh", overflow: "scroll", padding: "1rem 2rem", boxShadow: "5px 5px 5px gray", marginTop: "2rem", border: "1px solid gray" }}>
+                <ul>
+                  {
+                    keys.map(key => {
+                      return <EditInput key={key} label={key} value={fileData[key]} dispatch={dispatch} />
+                    })
+                  }
+                </ul>
+              </div>
+            </>
+          )
+        }
+
       </form>
     </>
   )
